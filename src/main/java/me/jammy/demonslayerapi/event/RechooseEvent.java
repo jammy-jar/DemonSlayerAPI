a3eed261afc69ed3a603d437f627870fe1ad3c21
jammy-jar <jrose04.0821@gmail.com>
2024-07-19 16:27:34 +0100

    Fixes

--- src/main/java/me/jammy/demonslayerapi/event/RechooseEvent.java
+++ src/main/java/me/jammy/demonslayerapi/event/RechooseEvent.java
@@ -2,7 +2,6 @@ package me.jammy.demonslayerapi.event;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 import org.jetbrains.annotations.NotNull;
